b0042012b1db2500fda90dfb347f9324d844a3f7
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-07-25 05:53:13 +0800

    init

---
+++ src/main/java/com/twistlet/mudahscrape/App.java
@@ -0,0 +1,13 @@
+package com.twistlet.mudahscrape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
